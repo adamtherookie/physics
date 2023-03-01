6844f722bef8874485c6de4bd7efc944fe4772f3
Adam Salhi <adamtherookie@proton.me>
2023-03-01 21:20:43 +0100

    Simpler build system

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+
+g++ main.cpp -o fiziks -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "raylib.h"
 
 int main(void) {
   std::cout << "Hello World!\n";
