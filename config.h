4d757b52f8b1107a7b1166f68ac478508b9ac5ab
Adam Salhi <adamtherookie@proton.me>
2023-03-03 20:44:16 +0100

    Cleaned up code

---
+++ config.h
@@ -0,0 +1,71 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// This file contains all of the configurations you need
+
+//
+// BEGIN DON'T TOUCH PART
+//
+// A 2D vector, has X and Y coords
+struct vec {
+  float x;
+  float y;
+};
+
+// An object, could be anything but we made it a circle because who 
+// doesn't like circles, plus it's easier to simulate circles because 
+// they only have 2 defining characteristics: their center and their radius
+// which leaves us tons of time to focus on the hard stuff instead of how
+// to render and calculate collisions and stuff between other shapes
+struct object {
+  vec position;
+  vec velocity;
+  vec force;
+
+  float radius;
+  float mass;
+  float bounciness;
+  
+  Color color;
+};
+
+//
+// END DON'T TOUCH PART
+//
+
+// Touch this part:
+const int screenWidth = 1366; // My monitor resolution, change it to fit yours
+const int screenHeight = 768;
+
+// Gravity on planet earth (change if you want to simulate physics elsewhere)
+vec gravity = { 0, 9.81 };
+
+float k = 1; // Friction on the screen edges
+
+// Define the objects you want to simulate here:
+object circle1 = {
+  { 500, 300 },  // position
+  { 10, -50 },   // velocity
+  { 0, 0 },      // force
+
+  20,            // radius
+  5,             // mass
+  0.6,
+
+  MAROON         // color
+};
+
+object circle2 = {
+  { 800, 300 },
+  { -50, -50 },
+  { 0, 0 },
+
+  50,
+  10,
+  0.65,
+
+  BLUE
+};
+
+#endif
+
--- main.cpp
+++ main.cpp
@@ -2,39 +2,9 @@
 #include <vector>
 #include <stdlib.h>
 
-const int screenWidth = 1366; // My monitor resolution, change it to fit yours
-const int screenHeight = 768; //
+#include "config.h"
 
-// A 2D vector, has X and Y coordinates 
-typedef struct vec {
-  float x;
-  float y;
-}vec;
-
-// An object, could be anything but we made it a circle because who 
-// doesn't like circles, plus it's easier to simulate circles because 
-// they only have 2 defining characteristics: their center and their radius
-// which leaves us tons of time to focus on the hard stuff instead of how
-// to render and calculate collisions and stuff between other shapes
-struct object {
-  vec position;
-  vec velocity;
-  vec force;
-
-  float radius;  
-  float mass;
-  float bounciness;
-
-  Color color;
-};
-
-// The universe (it's just a big collection of all the objects
-// we will be simulating in a 2D vector (in C++'s definintion not
-// the physics definition))
 std::vector<object *> universe;
-
-// Gravity on planet earth
-vec gravity = { 0, 9.81 };
 
 // The actual update function
 void step(float dt) {
@@ -45,34 +15,32 @@ void step(float dt) {
       obj->position.y = screenHeight - obj->radius; // reset the object to the top of the screen
                                                     // in case it went above and beyond
       obj->velocity.y *= -obj->bounciness; // bounce it off the wall according to its elasticity
-      obj->force.y += -1 * obj->velocity.y; // friction, we have f = k*v, and we chose k for the
-                                            // screen's edge to be -1, though it still seems a bit
-                                            // slippery, like its moving on ice. we should change that
-      obj->force.x += -1 * obj->velocity.x;
+      obj->force.y += -k * obj->velocity.y; // friction, we have f = k*v
+      obj->force.x += -k * obj->velocity.x;
     }
 
     // Check if the object went below the screen, and make it go back
     if(obj->position.y - obj->radius < 0) {
       obj->position.y = obj->radius;  // Same explanation as before
       obj->velocity.y *= -obj->bounciness; 
-      obj->force.y += -1 * obj->velocity.y;
-      obj->force.x += -1 * obj->velocity.x;
+      obj->force.y += -k * obj->velocity.y;
+      obj->force.x += -k * obj->velocity.x;
     }
 
     // Check if the object went off the right of the screen
     if(obj->position.x + obj->radius > screenWidth) {
       obj->position.x = screenWidth - obj->radius;
       obj->velocity.x *= -obj->bounciness;
-      obj->force.y += -1 * obj->velocity.y;
-      obj->force.x += -1 * obj->velocity.x;
+      obj->force.y += -k * obj->velocity.y;
+      obj->force.x += -k * obj->velocity.x;
     }
 
     // Check if the object went off the left of the screen
     if(obj->position.x - obj->radius < 0) {
       obj->position.x = obj->radius;
       obj->velocity.x *= -obj->bounciness;
-      obj->force.y += -0.1 * obj->velocity.y;
-      obj->force.x += -0.1 * obj->velocity.x;
+      obj->force.y += -k * obj->velocity.y;
+      obj->force.x += -k * obj->velocity.x;
     }
 
     // Add the effect of gravity to the Y-force, we have: P = m*g
@@ -102,32 +70,6 @@ int main(void) {
   InitWindow(screenWidth, screenHeight, "physics");
   SetTargetFPS(60);
 
-  // Define objects
-  object circle1 = {
-    { 500, 300 },  // position
-    { 50, -50 },    // velocity
-    { 0, 0 },    // force
-
-    20,          // radius
-    5,           // mass
-    0.6,
-
-    MAROON       // color
-  };
-
-  object circle2 = {
-    { 800, 300 },
-    { -50, -50 },
-    { 0, 0 },
-
-    50,
-    10,
-    0.2,
-
-    BLUE
-  };
-
-  // Add the objects to the universe (basically give birth to them)
   universe.push_back(&circle1);
   universe.push_back(&circle2);
 
